3c9d9ec2c5a6115f49ffa6109e8126a17fde47de
uni-3 <7pluto8@gmail.com>
2020-06-27 19:34:02 +0900

    modify icon

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,20 +11,13 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:art_genre_class/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(LogoWidget());
+  testWidgets('dom test', (WidgetTester tester) async {
+    // treeの構築
+    await tester.pumpWidget(MainApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // 初期画面テスト textがあるwidgetは1つ
+    expect(find.text('No Image Choosing'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // buttonテスト? image読み込んでclassifyする
   });
 }
